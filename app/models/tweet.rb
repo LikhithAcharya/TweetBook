6bdba6ff50c0a9f06181bece846966d502664b9e
likhith acharya <likhithacharya277@gmail.com>
2017-10-27 19:16:49 +0530

    Create tweets table and validations

---
+++ app/models/tweet.rb
@@ -0,0 +1,4 @@
+class Tweet < ActiveRecord::Base
+  validates :title, presence: true, length:{ minimum:3, maximum:50}
+  validates :description, presence: true, length:{ minimum:5, maximum:300}
+end
---
+++ db/migrate/20171027130706_create_tweets.rb
@@ -0,0 +1,9 @@
+class CreateTweets < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tweets do |t|
+      t.string :title
+      t.text :description
+      t.timestamps
+    end
+  end
+end
