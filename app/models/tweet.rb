3e780673ad57b0a3f3e382d951a72327c7cb3e0c
Likhith <likhithacharya277@gmail.com>
2017-11-04 17:22:40 +0530

    4 tables working fine

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,12 +1,13 @@
 class Tweet < ActiveRecord::Base
    belongs_to :user
    has_many :comments
+   
    has_many :tweet_categories
    has_many :categories, through: :tweet_categories
 
 
    default_scope { order(created_at: :desc)}
-   
+
    validates :title, presence: true, length:{ minimum:3, maximum:50}
    validates :description, presence: true, length:{ minimum:5, maximum:300}
 end
